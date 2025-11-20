938702937e5f090a0232d27275c25882abe6c6b9
kaio-leandro <kaiofacanha.ti@gmail.com>
2025-11-20 00:05:34 -0300

    feat: adicionand validações ao código

--- desafio-14.js
+++ desafio-14.js
@@ -5,6 +5,31 @@ function sortfunction (a, b){
     return (a - b);
 }
 
+function numberValidator (lengthArray, minValue, maxValue){
+    lengthArray = Number(lengthArray);
+    minValue = Number(minValue);
+    maxValue = Number(maxValue);
+
+    //Validação que impede letras, strings vazias ou valores inválidos
+    if(isNaN(lengthArray) || isNaN(minValue) || isNaN(maxValue)){
+        return ("Todos os valores devem ser número");
+    }
+    
+    //Valida se o tamanho da lista é um número inteiro positivo
+    if(lengthArray <= 0 || !Number.isInteger(lengthArray)){
+        return ("O tamanho do array deve ser um númeo inteiro positivo");
+    }
+
+    //Valida min < max
+    if(minValue >= maxValue){
+        return ("O valor mínimo deve ser menor que o valor máximo");
+    }
+
+    //Caso passe de todas as validações
+    return randomArray(lengthArray, minValue, maxValue);
+
+}
+
 function randomArray (lengthArray, minValue, maxValue){
     let array = [];
     for(let i = 0; i < lengthArray; i++){
